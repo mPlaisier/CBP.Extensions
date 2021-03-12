da68d5625382ccd8f44ad18aec1dc172270ad62a
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2021-03-12 19:37:13 +0100

    INIT

---
+++ CBP.Extensions/Decimal.Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CBP.Extensions
+{
+    public static class DecimalExtensions
+    {
+        /// <summary>
+        /// Returns a decimal with 2 decimal values.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public static string DefaultFormat(this decimal amount)
+        {
+            return String.Format("€ {0:0.00}", amount);
+        }
+    }
+}
---
+++ CBP.Extensions/Task.Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace CBP.Extensions
+{
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Execute a Task and Forget.
+        /// </summary>
+        /// <param name="_"></param>
+        public static void Forget(this Task _)
+        {
+            //Forget task and continue
+        }
+    }
+}
