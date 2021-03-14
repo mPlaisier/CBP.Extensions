fcc533dece9f2342827eeed488ab888850b2d8c9
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2021-03-14 10:14:02 +0100

    class extensions, check if null, check if not null

---
+++ CBP.Extensions.UnitTests/ClassExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CBP.Extensions.UnitTests
+{
+    public class ClassExtensionsTests
+    {
+        [Theory]
+        [InlineData("string", false)]
+        [InlineData(5, false)]
+        [InlineData(null, true)]
+        public void CheckIfObjectIsNull(object obj, bool isNull)
+        {
+            var result = obj.IsNull();
+
+            result.Should().Be(isNull);
+        }
+
+        [Theory]
+        [InlineData("string", true)]
+        [InlineData(5, true)]
+        [InlineData(null, false)]
+        public void CheckIfObjectIsNotNull(object obj, bool isNull)
+        {
+            var result = obj.IsNotNull();
+
+            result.Should().Be(isNull);
+        }
+    }
+}
---
+++ CBP.Extensions/Class.Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace CBP.Extensions
+{
+    public static class ClassExtensions
+    {
+        /// <summary>
+        /// Checks if the object is null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns>Returns true if the object is null</returns>
+        public static bool IsNull<T>(this T obj) where T : class
+        {
+            return obj == null;
+        }
+
+        /// <summary>
+        /// Checks if the object is not null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns>Returns true is the object is not null</returns>
+        public static bool IsNotNull<T>(this T obj) where T : class
+        {
+            return obj != null;
+        }
+    }
+}
