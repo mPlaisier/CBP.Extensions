425438ffb7e0d6574a3b42f48320606d7a3ae9a2
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2021-03-15 21:44:12 +0100

    add enum utils to get all values

---
+++ CBP.Extensions/EnumUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CBP.Extensions
+{
+    public static class EnumUtil
+    {
+        public static IEnumerable<T> GetValues<T>() where T : Enum
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+    }
+}
