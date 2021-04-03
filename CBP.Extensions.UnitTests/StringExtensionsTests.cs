8eade9d5c72524c27346b0f8c29fccee149fe3d2
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2021-04-03 17:32:45 +0200

    add string extensions with unit tests

---
+++ CBP.Extensions.UnitTests/StringExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace CBP.Extensions.UnitTests
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("abcd", 3)]
+        [InlineData("abcd", 0)]
+        [InlineData("abcdefghijklmno", 5)]
+        public void TruncateShouldAddDotsAtTheEnd(string value, int max)
+        {
+            string result = value.Truncate(max);
+
+            result.Should().EndWith("...");
+        }
+
+        [Theory]
+        [InlineData("abcd", 4)]
+        [InlineData("abcd", 5)]
+        [InlineData("abcdefghijklmno", 100)]
+        [InlineData("", 0)]
+        public void TruncateShouldNotAddDotsAtTheEnd(string value, int max)
+        {
+            string result = value.Truncate(max);
+
+            result.Should().NotEndWith("...");
+        }
+
+        [Fact]
+        public void TruncateThrowErrorIfStringIsNull()
+        {
+            string value = null;
+
+            Action act = () => value.Truncate(0);
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+    }
+}
