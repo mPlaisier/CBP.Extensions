a55abe70ada6f80a88d459f64e44837544910ed0
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2021-11-08 11:31:17 +0100

    #20 Method to capitalize first character of a string + Unit tests

--- CBP.Extensions/String.Extensions.cs
+++ CBP.Extensions/String.Extensions.cs
@@ -5,6 +5,7 @@ namespace CBP.Extensions
     public static class StringExtensions
     {
         const string STRING_NULL_ERROR = "string should not be null";
+        const string STRING_EMPTY_ERROR = "string cannot be empty";
 
         public static string Truncate(this string value, int maxChars)
         {
@@ -12,6 +13,16 @@ namespace CBP.Extensions
                 throw new ArgumentNullException(nameof(value), STRING_NULL_ERROR);
 
             return value.Length <= maxChars ? value : value.Substring(0, maxChars) + "...";
+        }
+
+        public static string FirstCharToUpper(this string input)
+        {
+            return input switch
+            {
+                null => throw new ArgumentNullException(nameof(input)),
+                "" => throw new ArgumentException(STRING_EMPTY_ERROR, nameof(input)),
+                _ => input[0].ToString().ToUpper() + input[1..]
+            };
         }
     }
 }
